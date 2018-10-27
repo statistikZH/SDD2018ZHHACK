0a576a4d5a8dc877ad24d5597d28210b38b809fc
tlo <tlorusso@users.noreply.github.com>
2018-10-27 20:25:42 +0200

    switch to server-side selectize input instead of client side

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -26,42 +26,42 @@ top_domain <- as.character(unique(data$top_domain))
 top10 <-data %>% top_n(10,Klicks)
 
 
-#does not work yet - button customizing
-# tags$style(".nav-tabs li.active a::focus {border-radius: 4px 4px 4px 4px ;border-bottom-color: #ffffff;}")),
-
-
 # UI ------------------
 ui <- fluidPage(
   theme = "bootstrap.css",
   tags$header(list(tags$style("img {display:inline-block;background-repeat:no-repeat;position:relative;left:10px;z-index:3;}"),
                    tags$a(href="http://www.zh.ch", tags$img(src="lionwhite.png", height="90%"), target="_blank")),
-                   tags$style("header {background-color: #009ee0 ;padding-top:10px;height:60px}")),
+              tags$style("header {background-color: #009ee0 ;padding-top:10px;height:60px}")),
   # Application title
   titlePanel("sear.zh"),
   "This shiny-App has been developed at the Swiss Digital Day - Hackathon 2018. It reveals which Google search keywords led people to the website of the canton of Zurich and its subdomains.
   It allows the visualization of relationships between search terms and domains of zh.ch. ",
   br(),
   # Show a plot of the generated distribution
-fluidRow(
-  column(4, selectizeInput('searchselect', label= 'search terms',choices = searchterms, 
-                         multiple = TRUE,selected = top10$Suchanfragen, options= list(maxItems = 10))),
-  column(4,  selectInput('domainselect', label='subdomain', choices=top_domain, selected = NULL, multiple = TRUE, selectize = TRUE))
-      ),
-column(12,"By checking the checkboy below you can decompose the search terms into single words."),
-column(4, checkboxInput("checkbox", label="single terms", value = FALSE)),
-column(4, checkboxInput("checkbox_year", label="include all domains (caution : varying timespans!)", value = FALSE)),
+  fluidRow(
+    column(4, selectizeInput('searchselect', label= 'search terms',choices = NULL,multiple=TRUE)),
+    #  multiple = TRUE,selected = c("kapo zh","kantonspolizei zürich","steueramt zürich","private tax 2017","steuererklärung 2017","steuerrechner zürich","quellensteuer zürich","kantonales steueramt zürich","steuererklärung zürich","steuern zürich"), options= list(maxItems = 10))),
+    column(4,  selectInput('domainselect', label='subdomain', choices=top_domain, selected = NULL, multiple = TRUE, selectize = TRUE))
+  ),
+  column(12,"By checking the checkbox below you can decompose the search terms into single words."),
+  column(4, checkboxInput("checkbox", label="single terms", value = FALSE)),
+  column(4, checkboxInput("checkbox_year", label="include all domains (caution : varying timespans!)", value = FALSE)),
+  column(12,"The flows reveal how many impressions of a site a search term has generated (transpartent flow) as well as clicks (saturated flow)."),
   
   mainPanel( plotlyOutput("sankey")  )
-
+  
 )
 
 
 # SERVER LOGIC  --------------------
 
-server <- function(input, output) {
+server <- function(input, output,session) {
   
+  
+  updateSelectizeInput(session=session, 'searchselect', choices = searchterms, server = TRUE, selected=top10$Suchanfragen)
   
   output$sankey <- renderPlotly({
+    ?updateSelectizeInput
     
     if(length(input$domainselect)==0){
       
@@ -73,7 +73,7 @@ server <- function(input, output) {
       
       zhweb(domain_filter(data,domains=input$domainselect,filter_yr=!input$checkbox_year)) 
     }
-      
+    
     
   })
   
